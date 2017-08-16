6ec338d5f257001472953985d1642859fb5099cd
Tyler Adams <tyleraadams@gmail.com>
2017-08-16 15:11:06 -0400

    [MISC] preoll bugfix

--- src/create-event-handlers/on-before-play.js
+++ src/create-event-handlers/on-before-play.js
@@ -1,7 +1,7 @@
 function onBeforePlay(event, player) {
   const currentVideo = player.getPlaylistItem();
 
-  if (!this.state.hasPlayed && typeof this.props.generatePrerollUrl === 'function') {
+  if (typeof this.props.generatePrerollUrl === 'function') {
     player.playAd(this.props.generatePrerollUrl(currentVideo));
   }
 }
