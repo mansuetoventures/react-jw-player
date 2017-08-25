46ae1d7bec610302c59df2c6e550db9bcfe0ca33
michaelbenin <michaellouisbenin@gmail.com>
2017-08-25 17:54:35 +0000

    [MISC] - dont play ad after pause

--- src/create-event-handlers/on-before-play.js
+++ src/create-event-handlers/on-before-play.js
@@ -2,6 +2,9 @@ function onBeforePlay(event, player) {
   const currentVideo = player.getPlaylistItem();
 
   if (typeof this.props.generatePrerollUrl === 'function') {
+    if (player.getState() === 'paused') {
+      return false;
+    }
     player.playAd(this.props.generatePrerollUrl(currentVideo));
   }
 }
